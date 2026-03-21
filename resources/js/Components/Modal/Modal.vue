866b5691e0c37bb549067a0b2493bc5417c2e8b8
MichaelRushton <mikerushton@hotmail.co.uk>
2026-03-21 12:18:43 +0000

    Make modals full width on mobile

--- resources/js/Components/Modal/Modal.vue
+++ resources/js/Components/Modal/Modal.vue
@@ -1,6 +1,6 @@
 <template>
     <dialog
-        class="border-neutral-default m-auto flex flex-col rounded border bg-white sm:w-xl"
+        class="border-neutral-default m-auto flex w-full flex-col rounded border bg-white sm:w-xl"
         @vue:mounted="(e) => e.el.showModal()"
         @close="$emit('closeModal')"
     >
